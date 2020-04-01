c8463ef37b07dd10f1ad4d2a309c684acdab671e
Rehan Madhugiri <rehan.madhugiri@gmail.com>
2020-04-01 11:39:12 -0400

    Added comments

--- BufMgr/src/buffer.cpp
+++ BufMgr/src/buffer.cpp
@@ -1,6 +1,7 @@
 /**
  * Nick Merfeld nmerfeld
  * Uilliam Lawless ulawless
+ * Rehan Madhugiri madhugiri
  * @author See Contributors.txt for code contributors and overview of BadgerDB.
  *
  * @section LICENSE
